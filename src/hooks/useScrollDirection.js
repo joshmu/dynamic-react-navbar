300436f2f8a9b10434b887aed62d22be99cefa4c
Josh Mu <mu@joshmu.com>
2020-04-30 18:19:04 +1000

    switched default for useScrollDirection

--- src/hooks/useScrollDirection.js
+++ src/hooks/useScrollDirection.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 
 const useScrollDirection = () => {
   const [scroll, setScroll] = useState(null)
-  const [scrollingDown, setScrollingDown] = useState(true)
+  const [scrollingDown, setScrollingDown] = useState(false)
 
   const handleScroll = () => {
     const newScroll = window.scrollY
